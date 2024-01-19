9fa5ebbcbb7f4906fc5e394ae87b3bc9bad676bd
jpeck <jzp@phoebussoftware.com>
2024-01-19 13:23:37 +0000

    Developed counting + part of ordering functions and prelimiary test case

--- src/main/java/TextProcessor.java
+++ src/main/java/TextProcessor.java
@@ -1,3 +1,48 @@
+import java.util.HashMap;
+
 public class TextProcessor {
-	
+
+  public HashMap<String, Integer> countTop10Words(String text) {
+    String[] wordArray = splitWords(text);
+
+    HashMap<String, Integer> top10 = new HashMap<>();
+
+    top10 = countWords(wordArray);
+
+    top10 = orderWords(top10);
+
+    System.out.println(top10.toString());
+    return top10;
+  }
+
+  private String[] splitWords(String text) {
+    return text.split(" ");
+  }
+
+  private HashMap<String, Integer> orderWords(HashMap<String, Integer> wordCount) {
+    String topWord = null;
+    for (String word : wordCount.keySet()) {
+      if (topWord == null) {
+        topWord = word;
+      }
+
+      if (wordCount.get(word) > wordCount.get(topWord)) {
+        topWord = word;
+      }
+    }
+  }
+
+  private HashMap<String, Integer> countWords(String[] wordArray) {
+    HashMap<String, Integer> runningTotal = new HashMap<>();
+    for (String word : wordArray) {
+      runningTotal.put(word, 0);
+    }
+
+    for (String word : wordArray) {
+      runningTotal.put(word, runningTotal.get(word) + 1);
+    }
+
+    return runningTotal;
+  }
+
 }
--- src/test/java/TextProcessorTests.java
+++ src/test/java/TextProcessorTests.java
@@ -1,10 +1,24 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashMap;
+
 import org.junit.jupiter.api.Test;
 
 public class TextProcessorTests {
-    @Test
-    public void Given_Preconditions_When_StateUnderTest_Then_ExpectedBehavior() {
-        // arrange
-        // act
-        // assert
-    }
+
+  @Test
+  public void Given_Preconditions_When_StateUnderTest_Then_ExpectedBehavior() {
+    TextProcessor textProcessor = new TextProcessor();
+
+    // arrange
+    String text = "Hello Hello";
+    HashMap<String, Integer> expectedTop10 = new HashMap<>();
+    expectedTop10.put("Hello", 2);
+
+    // act
+    HashMap<String, Integer> actualTop10 = textProcessor.countTop10Words(text);
+
+    // assert
+    assertTrue(expectedTop10.equals(actualTop10));
+  }
 }
