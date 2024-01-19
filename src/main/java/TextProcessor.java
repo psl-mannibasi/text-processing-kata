3f381fece980ffa2824d8a8908e30747efb7d196
Manni Basi <MB@phoebussoftware.com>
2024-01-19 13:30:42 +0000

    Code from CodeGym session

--- src/main/java/TextProcessor.java
+++ src/main/java/TextProcessor.java
@@ -1,3 +1,30 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TextProcessor {
-	
+
+    public static String textProcessing(String input) {
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("The top 10 words used include:");
+
+        String[] words = input.replaceAll("[^a-zA-Z0-9\\s]", "").split("\\s+");
+        System.out.println(Arrays.toString(words));
+        Map<String,Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            String lowerWord = word.toLowerCase();
+            if (map.containsKey(lowerWord)) {
+                Integer wordCount = map.get(lowerWord);
+                wordCount++;
+                map.put(lowerWord, wordCount);
+            } else {
+                map.put(lowerWord,1);
+            }
+        }
+        System.out.println(map);
+
+        return builder.toString();
+    }
 }
--- src/test/java/TextProcessorTests.java
+++ src/test/java/TextProcessorTests.java
@@ -3,8 +3,9 @@ import org.junit.jupiter.api.Test;
 public class TextProcessorTests {
     @Test
     public void Given_Preconditions_When_StateUnderTest_Then_ExpectedBehavior() {
-        // arrange
-        // act
-        // assert
+
+        String result = TextProcessor.textProcessing("Hello, this is an example for you to practice. You should grab this text and use it as your test case.");
+
+        System.out.println(result);
     }
 }
